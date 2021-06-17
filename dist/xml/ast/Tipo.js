28b815346756ac9d65f77ab5b2f4480c3ecd22dd
elder tojin <eldertojins@gmail.com>
2021-06-17 13:25:54 -0600

    [ADD] prueba2

--- dist/xml/ast/Tipo.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var Tipo;
-(function (Tipo) {
-    Tipo[Tipo["ATRIBUTO"] = 0] = "ATRIBUTO";
-    Tipo[Tipo["OBJETO"] = 1] = "OBJETO";
-    Tipo[Tipo["ENCODING"] = 2] = "ENCODING";
-})(Tipo || (Tipo = {}));
