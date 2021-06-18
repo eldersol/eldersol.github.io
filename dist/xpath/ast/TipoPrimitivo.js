905b4f43dafad7f0f8342f7d275363c40dbac726
Elder Tojin <eldertojins@gmail.com>
2021-06-18 12:42:38 -0600

    [RUN] prueba examen

--- dist/xpath/ast/TipoPrimitivo.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var TipoPrimitvo;
-(function (TipoPrimitvo) {
-    TipoPrimitvo[TipoPrimitvo["INT"] = 0] = "INT";
-    TipoPrimitvo[TipoPrimitvo["STRING"] = 1] = "STRING";
-    TipoPrimitvo[TipoPrimitvo["ESPECIAL"] = 2] = "ESPECIAL";
-    TipoPrimitvo[TipoPrimitvo["ID"] = 3] = "ID";
-    TipoPrimitvo[TipoPrimitvo["ATRIBUTO"] = 4] = "ATRIBUTO";
-})(TipoPrimitvo || (TipoPrimitvo = {}));
