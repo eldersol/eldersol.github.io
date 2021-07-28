04c566152663d8c22021fada1b8e38189d043025
Elder Tojin <eldertojins@gmail.com>
2021-07-28 14:16:55 -0600

    Crear proyecto jison

---
+++ jison/src/App.tsx
@@ -0,0 +1,12 @@
+import Contador from './Components/Contador';
+
+function App() {
+  return (
+    <div >
+      <h1>hola mundo!!!</h1>
+      <Contador/>      
+    </div>
+  );
+}
+
+export default App;
---
+++ jison/src/Components/Contador.tsx
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react'
+
+export interface ContadorProps {
+ 
+    
+}
+ 
+const Contador: React.SFC<ContadorProps> = () => {
+    return ( 
+        <Fragment>
+            <h2>hola contador</h2>
+        </Fragment>
+     );
+}
+ 
+export default Contador;
